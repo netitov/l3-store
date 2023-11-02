ab344a56f7ae98da4fc629e770fbff023de86451
Nikita Titov <netitov@gmail.com>
2023-11-02 19:24:12 +0100

    feat: event analytics

--- src/modules/productList/productList.ts
+++ src/modules/productList/productList.ts
@@ -3,14 +3,17 @@ import { View } from '../../utils/view';
 import html from './productList.tpl.html';
 import { ProductData } from 'types';
 import { Product } from '../product/product';
+import { analyticsService } from '../../services/analytics.service';
 
 export class ProductList {
   view: View;
   products: ProductData[];
+  observer: IntersectionObserver;
 
   constructor() {
     this.products = [];
     this.view = new ViewTemplate(html).cloneView();
+    this.observer = new IntersectionObserver(this._observeProductView, { threshold: 1.0 });
   }
 
   attach($root: HTMLElement) {
@@ -30,6 +33,31 @@ export class ProductList {
       const productComp = new Product(product);
       productComp.render();
       productComp.attach(this.view.root);
+
+      this.observer.observe(productComp.view.root);
     });
   }
+
+  private _observeProductView = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const productElement = entry.target as HTMLElement;
+
+        const productId = Number(productElement.dataset.productId);
+        const productData = this.products.find(i => i.id === productId);
+
+        if (productData) {
+          const eventType = productData && Object.keys(productData.log).length > 0 ? 'viewCardPromo' : 'viewCard';
+
+          //fetch secret key
+          fetch(`/api/getProductSecretKey?id=${productId}`)
+            .then((res) => res.json())
+            .then((secretKey) => {
+              analyticsService.sendAlaytics(eventType, { ...productData, secretKey });
+            });
+        }
+      }
+    });
+  }
+
 }
